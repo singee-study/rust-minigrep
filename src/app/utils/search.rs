babb4380d0cb6ccfada4d121802f5d17b0ade8e9
Singee <git@singee.me>
2020-08-29 17:32:24 +0800

    add test for search

--- src/app/utils/search.rs
+++ src/app/utils/search.rs
@@ -9,3 +9,38 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
 
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        assert_eq!(
+            vec!["a"],
+            search(
+                "a",
+                "\
+a
+b
+c
+d"
+            )
+        )
+    }
+
+    #[test]
+    fn two_result() {
+        assert_eq!(
+            vec!["a", "ab"],
+            search(
+                "a",
+                "\
+a
+ab
+bc
+cd"
+            )
+        )
+    }
+}
